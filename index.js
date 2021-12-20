2b244a1ee7dc184e016e0ffb8d001bca3976c6ad
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2021-12-20 17:20:31 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	
+});
+
+http.listen(80);
